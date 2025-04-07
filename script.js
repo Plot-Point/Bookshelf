736793192ee3c6998bbcc4a581d4d23dcf807306
Plot-Point <makayla.lesage@gmail.com>
2025-04-07 15:04:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+    /*function handleImagePreview(inputId, previewId) {
+    document.getElementById(inputId).addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                const preview = document.getElementById(previewId);
+                preview.src = e.target.result;
+                preview.style.display = 'block';
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+}
+
+// Call the image preview function for each input
+handleImagePreview('imageInputFinished', 'previewFinished');
+
+
+handleImagePreview('imageInputTBR', 'previewTBR');
+handleImagePreview('imageInputDNF', 'previewDNF');
+*/
+
+
 var modal = document.getElementById("myModal");
 var addBookBtn = document.getElementById("addBookBtnF");
 var span = document.getElementsByClassName("close")[0];
@@ -24,7 +48,6 @@ document.getElementById("bookForm").onsubmit = function(event) {
 
     var title = document.getElementById("bookTitle").value;
     var author = document.getElementById("bookAuthor").value;
-    var description = document.getElementById("bookDescription").value;
     var imageUrl = document.getElementById("bookImage").value;
     var rating = document.getElementById("bookRating").value;
 
@@ -33,10 +56,9 @@ document.getElementById("bookForm").onsubmit = function(event) {
     bookDiv.innerHTML = `
 		<center>
             <img alt="Book" src="${imageUrl}" width="175" height="250"/>
-            <h5 id="bookTitle">${title} by ${author}</h5>
-            <p id="desText">${description}</p>
-            <p>${rating}★</p>
-            <a class="moreBtn" href="#">Read More</a>
+            <h5>${title} by ${author}</h5>
+            <p>${rating}★</p><br>
+            <a class="moreBtn" href="more.html">Read More</a>
         </center>
     `;
 
